8fcb36103fbd75e8c5b212abed0658fc0e17793a
eudondian <eudondian@gmail.com>
2023-06-19 19:53:25 +0100

    Create Student class

---
+++ student.rb
@@ -0,0 +1,17 @@
+require './person'
+
+class Student < Person
+  def initialize(age, name = 'unknown', classroom:, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+
+  def add_classroom(classroom)
+    @classroom.student = classroom
+    classroom.students.push(self) unless classroom.students.iclude?(self)
+  end
+end
