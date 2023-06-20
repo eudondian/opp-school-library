ff0f023dce5c7006599efb051da183a7b4298a39
eudondian <eudondian@gmail.com>
2023-06-20 21:49:57 +0100

    Create rental class

---
+++ rental.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Rental
+  attr_accessor :date, :book, :person
+
+  def initialize(date, book, person)
+    @date = date
+    @person = person
+    @book = book
+
+    person.rentals << self
+    book.rentals << self
+  end
+end
